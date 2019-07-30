38115d18fb9658d7e5da27f03a66128510b1f4f2
optie <optie96@gmail.com>
2019-07-30 21:04:59 +0900

    update main

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ def main():
     texdir = './tex/'
     texs = os.listdir(texdir)
     texs.sort()
-    img = cv2.imread(texdir + texs[1])
 
-    textureOptimization = TextureOptimization(0)
-    out = np.zeros((512, 512, 3))
-    result = textureOptimization.synthesis(img, out, 12)
+    ws = [2, 4, 8, 16]
+    Ow = 256
+    Oh = 256
+    for w in ws:
+        for i, tex in enumerate(texs):
+            img = cv2.imread(texdir + texs[i])
 
-    name = texs[0].split('.')[0] + '_result.jpg'
-    cv2.imwrite('./result/' + name, result)
+            textureOptimization = TextureOptimization(0)
+            out = np.zeros((Ow, Oh, 3))
+            result = textureOptimization.synthesis(img, out, w)
+
+            name = texs[i].split('.')[0]
+            prefix = '{0}_{1}x{2}_b{3}'.format(name, Ow, Oh, w * 2 + 1)
+
+            textureOptimization.animation.save(prefix + '_anim.gif', writer="imagemagick")
+
+            outname = prefix + '_result.jpg'
+            cv2.imwrite('./result/' + outname, result)
 
 
 if __name__ == '__main__':
