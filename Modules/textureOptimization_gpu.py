4f192bae7996369f549f618cf11fccd17e515045
optie <optie96@gmail.com>
2019-08-11 02:10:12 +0900

    make code runnable

--- Modules/textureOptimization_gpu.py
+++ Modules/textureOptimization_gpu.py
@@ -1,24 +1,23 @@
-import cupy as cp
-from cupy.lib.stride_tricks import as_strided
-import cupyx.scipy.sparse as cpsp
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+import scipy.sparse as spsp
 from sklearn.decomposition import PCA
-import faiss
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import faiss
 
 
 class TextureOptimization:
     def __init__(self, tex):
-        self.Z = [cp.array(tex)]
+        self.Z = tex
         self.history = []
 
     def synthesis(self, Z, X, W, init=True, dimMax=100):
         """
-        Z: icput texture (r,c,ch)
+        Z: input texture (r,c,ch)
         X: output (r,c,ch)
         W: width of a patch
         """
-        X = cp.array(X)
         SynthInfo = {}
         step = W // 2
 
@@ -31,6 +30,7 @@ class TextureOptimization:
             ch
         )
         Z_strides = Z.strides[:2] + Z.strides
+
         # Z から取りうるすべてのブロック (w*w*ch) を縦横に並べた五次元配列
         blocks = as_strided(Z, Z_viewSize, Z_strides)
         r, c = blocks.shape[:2]
@@ -42,29 +42,34 @@ class TextureOptimization:
         self.pca = None
         if p_dim > dimMax:
             self.pca = PCA(n_components=dimMax)
-            DB = self.pca.fit_transform(allBlockVecs)
+            self.pca = self.pca.fit(allBlockVecs)
+            DB = self.pca.transform(allBlockVecs)
+            print('dim. reduction: {0} -> {1}'.format(p_dim, dimMax))
         else:
             DB = allBlockVecs
 
         SynthInfo['N'] = N
         SynthInfo['D'] = min(p_dim, dimMax)
-        print('Search Space: N={0}, D={1} ', SynthInfo['N'], SynthInfo['D'])
+        print('Search Space: N={0}, D={1} '.format(
+            SynthInfo['N'], SynthInfo['D']))
 
         res = faiss.StandardGpuResources()
         flat_config = faiss.GpuIndexFlatConfig()
         index = faiss.GpuIndexFlatL2(res, min(p_dim, dimMax), flat_config)
-        index.add(cp.asnumpy(allBlockVecs))
+        index.add(DB.astype('float32'))
+        print('index added')
 
         Xr, Xc = X.shape[:2]
-        p_rowRange = cp.arange(0, Xr, step)
-        p_colRange = cp.arange(0, Xc, step)
-        p_rowRange[-1] = min(p_rowRange[-1], Xr - step - 1)
-        p_colRange[-1] = min(p_colRange[-1], Xc - step - 1)
+        p_rowRange = np.arange(0, Xr, step)
+        p_colRange = np.arange(0, Xc, step)
+        p_rowRange[-1] = min(p_rowRange[-1], Xr - W - 1)
+        p_colRange[-1] = min(p_colRange[-1], Xc - W - 1)
         row_p_num = len(p_rowRange)
         col_p_num = len(p_colRange)
         Q = row_p_num * col_p_num
 
-        CoefMat = cp.zeros((Q * p_dim, Xr * Xc * ch))
+        CoefMat = spsp.lil_matrix((Q * p_dim, Xr * Xc * ch))
+        print('coefMat size: ({0},{1})'.format(Q * p_dim, Xr * Xc * ch))
 
         for (i, pos_y) in enumerate(p_rowRange):
             for (j, pos_x) in enumerate(p_colRange):
@@ -74,37 +79,41 @@ class TextureOptimization:
                             row_ix = i * row_p_num + j * col_p_num + k * W + l * W + m
                             col_ix = (pos_y + k) * Xr + (pos_x + l) + m
                             CoefMat[row_ix, col_ix] = 1
-        A = cpsp.csr_matrix(CoefMat)
+        A = spsp.csr_matrix(CoefMat)
         SynthInfo['Q'] = Q
-        print('query size: Q=', Q)
+        print('query size: Q =', Q)
 
-        ix_mat = cp.zeros((Q, p_dim))
-        ix = cp.arange(Xr * Xc * ch).reshape(Xr, Xc, ch)
+        ix_mat = np.zeros((Q, p_dim), dtype=int)
+        ix = np.arange(Xr * Xc * ch, dtype=int).reshape(Xr, Xc, ch)
         for (i, pos_y) in enumerate(p_rowRange):
             for (j, pos_x) in enumerate(p_colRange):
-                ix_mat[i * col_p_num + j, :] = ix[(pos_y - W):(pos_y + W + 1),
-                                                  (pos_x - W):(pos_x + W + 1)].flatten()
+                try:
+                    ix_mat[i * col_p_num + j, :] = ix[pos_y:(pos_y + W),
+                                                      pos_x:(pos_x + W)].flatten()
+                except ValueError:
+                    print(ix[pos_y:(pos_y + W),
+                             pos_x:(pos_x + W)].shape)
 
-        Ix = cp.zeros(Q)
+        Ix = np.zeros(Q, dtype=int)
         itr = 0
         fig = plt.figure(figsize=(5, 5))
         ims = []
         SynthInfo['iteration'] = []
         while True:
             # Maximization: find nearest {z_p}
-            Query = X[ix_mat]
+            Query = X.flatten()[ix_mat]
             if p_dim > dimMax:
-                Query = self.pca.fit_transform(Query)
+                Query = self.pca.transform(Query)
 
-            _D, Ix_next = index.search(Query, 1)
+            _D, Ix_next = index.search(Query.astype('float32'), 1)
 
-            if (Ix == Ix_next) | (itr > 100):
+            if np.all(Ix == Ix_next) | (itr > 100):
                 break
-            Ix = cp.copy(Ix_next)
+            Ix = np.copy(Ix_next)
 
             # Expectation: update x
-            b = DB[Ix].flatten()
-            sol = cpsp.linalg.lsqr(A, b)[0]
+            b = allBlockVecs[Ix].flatten()
+            sol, istop, itn, norm = spsp.linalg.lsmr(A, b)[:4]
             X = sol.reshape(Xr, Xc, -1)
             itr += 1
 
@@ -112,7 +121,8 @@ class TextureOptimization:
                 'int').clip(0, 255), animated=True)
             ims.append([im])
 
-            itrInfo = {"itr": itr, "log energy": cp.log(_D.sum())}
+            itrInfo = {"itr": itr, "log energy": norm,
+                       "lsmr istop": istop, "lsmr iter": itn}
             SynthInfo['iteration'].append(itrInfo)
             print(itrInfo)
 
@@ -122,4 +132,4 @@ class TextureOptimization:
         print('- synthesis converged')
         self.history.append(SynthInfo)
 
-        return cp.asnumpy(X)
+        return X
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Modules.textureOptimization_gpu import TextureOptimization
 import os
 import numpy as np
 import cv2
+import datetime
 
 
 def main():
@@ -10,24 +11,28 @@ def main():
     texs.sort()
 
     ws = [16, 32, 64]
-    Ow = 256
-    Oh = 256
+    Ow = 1024
+    Oh = 1024
     for w in ws:
         for i, tex in enumerate(texs):
-            img = cv2.imread(texdir + texs[i])
+            print(datetime.datetime.now().isoformat())
+            img = cv2.imread(texdir + tex)
 
-            textureOptimization = TextureOptimization(0)
+            textureOptimization = TextureOptimization(img)
             out = np.random.rand(Ow, Oh, 3) * 255
             result = textureOptimization.synthesis(img, out, w)
 
             name = texs[i].split('.')[0]
-            prefix = '{0}_{1}x{2}_b{3}'.format(name, Ow, Oh, w * 2 + 1)
+            prefix = '{0}_{1}x{2}_b{3}'.format(name, Ow, Oh, w)
 
             textureOptimization.animation.save(
                 './process/' + prefix + '_anim.gif', writer="imagemagick")
 
             outname = prefix + '_result.jpg'
             cv2.imwrite('./result/' + outname, result)
+
+            print(textureOptimization.history)
+            print(datetime.datetime.now().isoformat())
 
 
 if __name__ == '__main__':
